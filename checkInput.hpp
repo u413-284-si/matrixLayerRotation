f2c87eaab6289de1d2b95db661a590f035b29dd8
sirui <sirui.qiu01@gmail.com>
2024-05-12 11:58:43 +0200

    refactor: remove non ASCII chars

--- checkInput.hpp
+++ checkInput.hpp
@@ -11,10 +11,10 @@
 // Error definitions
 #define INV_ARG_NUM "invalid number of arguments provided\nexpected: nRows nCols nRotations"
 #define INV_MTX_DIM "invalid matrix dimension provided\nexpected: 2 <= nRows, nCols <= 300"
-#define INV_ROT_DIM "invalid rotation dimension provided\nexpected: 1 <= nRotations <= 10⁹"
+#define INV_ROT_DIM "invalid rotation dimension provided\nexpected: 1 <= nRotations <= 10^9"
 #define INV_MTX_MIN "invalid matrix dimension provided\nexpected: min(nRows, nCols) to be even"
 #define INV_ROW_SZ	"invalid number of elements in row"
-#define INV_ELEM_DIM "invalid matrix element dimension provided\nexpected: 1 <= element <= 10⁸"
+#define INV_ELEM_DIM "invalid matrix element dimension provided\nexpected: 1 <= element <= 10^8"
 
 /* ====== DECLARATIONS ====== */
 void	checkMatrixConditions(const std::vector<std::string>& input, int& nRows, int& nCols, int& nRotations);
